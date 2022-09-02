7359d73bdbfe0f3a5023dc7773d16890b1fb5bf1
Anton Gavryukhin <ah21@mail.ru>
2022-09-02 23:33:12 +0300

    Addiction Junit4 commit

--- src/test/java/ru/netology/servise/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -1,28 +1,35 @@
 package ru.netology.servise;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
+
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void purchaseAmountLessThan1000(){
-
-        int amount = 900;
+        int amount =888;
         int actual = service.remain(amount);
-        int expected = 100;
-        assertEquals(actual,expected);
+        int expected = 112;
+        assertEquals(expected,actual);
     }
     @Test
-    public void purchaseAmount1000 (){
-
+    public void purchaseAmount1000(){
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 100;
-        assertEquals(actual,expected);
+        int expected = 0;
+        assertEquals(expected,actual);
     }
+    @Test
+    public void purchaseAmountOver1000(){
+        int amount = 2222;
+        int actual = service.remain(amount);
+        int expected = 778;
+        assertEquals(expected,actual);
+    }
+
+}/*
+
     @Test
     public void purchaseAmountOver1000 (){
 
@@ -31,4 +38,4 @@ public class CashbackHackServiceTest {
         int expected = 900;
         assertEquals(actual,expected);
     }
-}
+}*/
