58635ac6305253d336b89fd76f24f9faf46791a9
Anton Gavryukhin <ah21@mail.ru>
2022-09-02 22:31:54 +0300

    Addiction TestTG commit

---
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.servise;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void purchaseAmountLessThan1000(){
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void purchaseAmount1000 (){
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void purchaseAmountOver1000 (){
+
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual,expected);
+    }
+}
