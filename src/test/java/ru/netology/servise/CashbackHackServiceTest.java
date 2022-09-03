553cda57b8feb49cd495318c9e165316f0739375
Anton Gavryukhin <ah21@mail.ru>
2022-09-03 11:52:37 +0300

    Task2 JUnit5 и Legacy

--- src/test/java/ru/netology/servise/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.servise;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
@@ -28,14 +29,29 @@ public class CashbackHackServiceTest {
         assertEquals(expected,actual);
     }
 
-}/*
 
-    @Test
-    public void purchaseAmountOver1000 (){
 
-        int amount = 1100;
-        int actual = service.remain(amount);
-        int expected = 900;
-        assertEquals(actual,expected);
+   @org.junit.jupiter.api.Test
+    public void junitPurchaseAmountOver1000 (){
+        CashbackHackService service1 = new CashbackHackService();
+        int amount = 1900;
+        int expected = 100;
+        int actual = service1.remain(amount);
+       Assertions.assertEquals(expected,actual);
     }
-}*/
+    @org.junit.jupiter.api.Test
+    public void junitPurchaseAmount1000(){
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void junitPurchaseAmountLessThan1000(){
+        int amount = 800;
+        int expected = 200;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+    }
+
+}
